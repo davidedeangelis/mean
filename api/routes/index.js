e00a8bebf9b06098c2a8fb7c8a81be6bc3446bef
davidedeangelis <davide.deangelis@gmail.com>
2017-06-26 21:06:18 +0100

    sectin 1 lesson 13 dynamic get by id in express

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,5 +6,7 @@ var ctrlHotels = require('../controllers/hotels.controllers.js');
 //define route for /json path
 router.route('/hotels').get(ctrlHotels.hotelsGetAll);
 
+router.route('/hotels/:hotelId').get(ctrlHotels.hotelsGetOne);
+
 //public access
 module.exports = router;
