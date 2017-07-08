60644275e9f08feb70428167f48c37286b2a8bae
davidedeangelis <davide.deangelis@gmail.com>
2017-07-08 15:28:34 +0100

    C3L30_31_GEOQUER_API_HARDENING

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,61 +1,172 @@
 var mongoose = require('mongoose');
 
-//GET CONTROLLER
-var hotelsGetAll = function(req, res) {
-//	var models = mongoose.modelNames()
-//	console.log("MODELS: ", models);
+var runGeoQuery = function(req, res) {
 	var Hotel = mongoose.model('Hotel');
-	
+	var lng = parseFloat(req.query.lng);
+	var lat = parseFloat(req.query.lat);
+
+	if (isNaN(lng) || isNaN(lat)) {
+		res
+		.status(400)
+		.json({
+			"message" : "If supplied in querystring, lng and lat must both be numbers"
+		});
+		return;
+	}
+
+	// A geoJSON point
+	var point = {
+			type : "Point",
+			coordinates : [lng, lat]
+	};
+
+	var geoOptions = {
+			spherical : true,
+			maxDistance : 2000,
+			num : 5
+	};
+
+	Hotel
+	.geoNear(point, geoOptions, function(err, results, stats) {
+		console.log('Geo Results', results);
+		console.log('Geo stats', stats);
+		if (err) {
+			console.log("Error finding hotels");
+			res
+			.status(500)
+			.json(err);
+		} else {
+			res
+			.status(200)
+			.json(results);
+		}
+	});
+};
+
+var hotelsGetAll = function(req, res) {
+	var Hotel = mongoose.model('Hotel');
+
+	console.log('GET the hotels');
+	console.log(req.query);
+
 	var offset = 0;
 	var count = 5;
-	
-	if(req.query && req.query.offset){
+	var maxCount = 50;
+
+	if (req.query && req.query.lat && req.query.lng) {
+		runGeoQuery(req, res);
+		return;
+	}
+
+	if (req.query && req.query.offset) {
 		offset = parseInt(req.query.offset, 10);
 	}
-	//check param existence and parse to int
-	if(req.query && req.query.count){
+
+	if (req.query && req.query.count) {
 		count = parseInt(req.query.count, 10);
 	}
-	
-	Hotel.find().skip(offset).limit(count).exec(function(err, hotels){
-		console.log('FOUND HOTELS', hotels.length);
-		res.json(hotels);
+
+	if (isNaN(offset) || isNaN(count)) {
+		res
+		.status(400)
+		.json({
+			"message" : "If supplied in querystring, count and offset must both be numbers"
+		});
+		return;
+	}
+
+	if (count > maxCount) {
+		res
+		.status(400)
+		.json({
+			"message" : "Count limit of " + maxCount + " exceeded"
+		});
+		return;
+	}
+
+	Hotel
+	.find()
+	.skip(offset)
+	.limit(count)
+	.exec(function(err, hotels) {
+		console.log(err);
+		console.log(hotels);
+		if (err) {
+			console.log("Error finding hotels");
+			res
+			.status(500)
+			.json(err);
+		} else {
+			console.log("Found hotels", hotels.length);
+			res
+			.json(hotels);
+		}
 	});
-}
+
+};
 
 var hotelsGetOne = function(req, res) {
-	var hotelId = req.params.hotelId;
+	var id = req.params.hotelId;
 	var Hotel = mongoose.model('Hotel');
-	console.log('HOTEL ID: ', hotelId);
-	
-	Hotel.findById(hotelId).exec(function(err, doc){
-		res.status(200);
-		res.json(doc);
+
+	console.log('GET hotelId', id);
+
+	Hotel
+	.findById(id)
+	.exec(function(err, doc) {
+		var response = {
+				status : 200,
+				message : doc
+		};
+		if (err) {
+			console.log("Error finding hotel");
+			response.status = 500;
+			response.message = err;
+		} else if(!doc) {
+			console.log("HotelId not found in database", id);
+			response.status = 404;
+			response.message = {
+					"message" : "Hotel ID not found " + id
+			};
+		}
+		res
+		.status(response.status)
+		.json(response.message);
 	});
-}
+
+};
 
 var hotelsAddOne = function(req, res) {
-	var db = dbConn.get();
+	console.log("POST new hotel");
+	var Hotel = mongoose.model('Hotel');
+	var db = dbconn.get();
 	var collection = db.collection('hotels');
 	var newHotel;
-	console.log('POST NEW HOTEL');
-	
+
 	if (req.body && req.body.name && req.body.stars) {
 		newHotel = req.body;
 		newHotel.stars = parseInt(req.body.stars, 10);
-		collection.insertOne(newHotel, function(err, response){
-			console.log(response.ops);
-			res.status(201);
-			res.json(response);
+		collection.insertOne(newHotel, function(err, response) {
+			console.log("Hotel added", response);
+			console.log("Hotel added", response.ops);
+			res
+			.status(201)
+			.json(response.ops);
 		});
+		// console.log(newHotel);
+		// res
+		//   .status(200)
+		//   .json(newHotel);
 	} else {
-		console.log('DATA MISSING FROM BODY');
-		res.status(400);
-		res.json({message : 'REQUEST DATA MISSING FROM BODY'});
+		console.log("Data missing from body");
+		res
+		.status(400)
+		.json({
+			message: "Required data missing from body"
+		});
 	}
-}
 
-
+};
 
 //make it public? mha...
 module.exports = {
